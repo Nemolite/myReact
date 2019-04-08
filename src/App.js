3b7a368e500b1e9d435e0d44b66f7ecb6d7e7032
Nemolite <g25092011@mail.ru>
2019-04-08 15:16:29 +0300

    component

--- src/App.js
+++ src/App.js
@@ -2,25 +2,25 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class Item extends Component {
+            render() {
+              return (<li>{this.props.name}</li>);
+            }
+          }
+
+
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <div>         
+                  <h2>{this.props.title}</h2>
+                  <ul>
+                      <Item name="iPhone 7" />
+                      <Item name="HTC U Ultra" />
+                      <Item name="Google Pixel" />
+                  </ul>
+              </div>
     );
   }
 }
